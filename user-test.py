b7646165714f7b7aa5dc3da01ebda6cf40aa57eb
BECKY-OCHOLLA <becky.ocholla@student.moringaschool.com>
2022-04-25 16:56:30 +0300

    it will check if the object has been initialized properly

--- user-test.py
+++ user-test.py
@@ -1,3 +1,4 @@
+from typing_extensions import Self
 import unittest #importing the unittest module
 from user import user #importing the user class
 
@@ -14,3 +15,11 @@ def setUp(self):
     set up method to run before each test case
     '''
     self.new_User = user("Becky","Ocholla","2022") # create contact object
+
+def test_init_(self):
+    '''
+    test if the objects have been initialized properly
+    '''
+    self.assertEqual(self.new_user.user_name,"Becky")
+    self.assertEqual(self.new_user.password,"2022")
+    

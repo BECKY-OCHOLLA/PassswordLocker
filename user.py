47fc870b7fad205e20b49afc6b116971bb3564b9
BECKY-OCHOLLA <becky.ocholla@student.moringaschool.com>
2022-04-25 18:48:36 +0300

    method used to save user in user lists

--- user.py
+++ user.py
@@ -6,11 +6,16 @@ class user:
 
 def __init__(self,first_name,last_name,user_name,password):
     '''
-    __init__ method that helps us define properties for our objects.
+    __init__ method that helps us define properties for our users.
     '''
         
     self.first_name = first_name
     self.last_name =last_name
     self.user_name = user_name
     self.password = password
-    
+
+def save_user(self):
+    '''
+    save_user method saves user intouserlists
+    '''  
+    user.userslists.append(self)  

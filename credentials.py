c7cf9dff4268ce8d9b2c5e5c3337dae35e0677db
BECKY-OCHOLLA <becky.ocholla@student.moringaschool.com>
2022-04-25 19:41:49 +0300

    add method that saves account

--- credentials.py
+++ credentials.py
@@ -8,4 +8,13 @@ def __init__(self,accountusername,accountname,accountpassword):
     '''
     _init_ methot that help use to define properties for object self
     '''
-    
+    self.accountusername = accountusername
+    self.accountname = accountname
+    self.accountpassword = accountpassword
+
+def save_account(self):
+    '''
+    save_account method saves user intouserlists
+    '''  
+
+    credentials.accounts.append(self)

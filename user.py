99d62f0236563771e136855c15409c098cd69677
BECKY-OCHOLLA <becky.ocholla@student.moringaschool.com>
2022-04-25 18:19:27 +0300

    it will initialize a method that helps to define propertiesof an object

--- user.py
+++ user.py
@@ -3,3 +3,14 @@ class user:
     A class thatbgenerates new instance of users
     '''
     userslists=[]
+
+def __init__(self,first_name,last_name,user_name,password):
+    '''
+    __init__ method that helps us define properties for our objects.
+    '''
+        
+    self.first_name = first_name
+    self.last_name =last_name
+    self.user_name = user_name
+    self.password = password
+    

bc92a8e0fa18047f67bc99bfb9ecff70c4218f43
BECKY-OCHOLLA <becky.ocholla@student.moringaschool.com>
2022-04-26 01:40:46 +0300

    it will add what is to be excecuted if a user chooses to SU

--- run.py
+++ run.py
@@ -18,7 +18,7 @@ def find_user(number):
 
 
 def create_account(accountusername,accountname,accountpassword):
-  newaccount= credentials(accountusername,accountname,accountpassword):
+  newaccount= credentials(accountusername,accountname,accountpassword)
   return newaccount
 
 def save_account(user):
@@ -29,3 +29,39 @@ def delete_account(user):
     
 def find_account(number):
     return credentials.find_by_number(number)
+
+
+def main():
+    while True:
+        print("Karibu passwordLocker andika SU au LI ili uweze kuendelea")
+        print("SU -au- LI")
+
+    if option =="SU":
+        print("Create Account")
+        print("-"*7)
+        print("Enter your First Name")
+        firstname=(input)
+
+        print("Enter your Last Name")
+        lastname=(input)
+
+        print("Enter your User Name")
+        username=(input)
+
+        print("set your  Password")
+        userpassword=(input)
+
+
+        save_user(create_user(firstname,lastname,username,userpassword))
+        print("your account was succesfully created below are your credentials")
+        print("-"*7)
+
+        print(f'Name:{firstname}{lastname}\nUsername{username}\nPassword{userpassword}')
+        print("\nUse login to your account using your credentials")
+        print("\n \n ")
+
+    elif option =="LI":
+        
+        
+        
+
